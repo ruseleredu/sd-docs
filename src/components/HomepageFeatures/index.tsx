25bb0cf9e7598256b2b2f973e54e31dff695f347
Adriano Ruseler <ruseler@utfpr.edu.br>
2025-11-07 13:36:10 -0300

    Update feature icon and SVG for homepage
    Replaced the 'Easy to Use' feature icon in HomepageFeatures with a new SVG and updated the artificial-intelligenceai SVG file with a version containing additional metadata and formatting improvements.

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -10,27 +10,7 @@ type FeatureItem = {
 };
 
 const FeatureList: FeatureItem[] = [
-  {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/teacher-female-svgrepo-com.svg').default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/artificial-intelligenceai-svgrepo-com.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
+   {
     title: 'Easy to Use',
     Svg: require('@site/static/img/class-svgrepo-com.svg').default,
     description: (
