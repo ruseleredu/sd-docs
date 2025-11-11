5738e8c3eeedf81fba0e9882d990013bbe785fb5
Adriano Ruseler <ruseler@utfpr.edu.br>
2025-11-11 14:52:08 -0300

    Add quizzes Q11–Q15 and update quizData content
    Added documentation files for quizzes Q11 to Q15 and updated src/data/quizData.ts with new quiz entries and revised descriptions and links for all quizzes. This improves coverage and accuracy of quiz information for the course.

--- src/data/quizData.ts
+++ src/data/quizData.ts
@@ -1,111 +1,149 @@
 // src/data/quizData.ts
 export type QuizRow = {
-    quiz: string;
+  quiz: string;
 
-    // internal/external quiz links
-    hrefi?: string; // internal (/quiz/quiz00)
-    hrefe?: string; // external (Moodle, etc.)
+  // internal/external quiz links
+  hrefi?: string; // internal (/quiz/quiz00)
+  hrefe?: string; // external (Moodle, etc.)
 
-    start: string;
-    end: string;
-    descricao: string;
+  start: string;
+  end: string;
+  descricao: string;
 };
 
 export const quizData: QuizRow[] = [
-    {
-        quiz: "Q00",
-        hrefi: "/ead/Q00",
-        hrefe: "https://moodle.utfpr.edu.br/mod/quiz/view.php?id=1957579",
-        start: "18-Aug-2025",
-        end: "25-Aug-2025",
-        descricao: "Apresentação da Disciplina; Materiais utilizados na disciplina;",
-    },
-    {
-        quiz: "Q01",
-        hrefi: "/ead/Q01",
-        hrefe: "https://moodle.utfpr.edu.br/mod/quiz/view.php?id=1756081",
-        start: "25-Aug-2025",
-        end: "01-Sep-2025",
-        descricao: "Revisão da programação em C;",
-    },
-    {
-        quiz: "Q02",
-        hrefi: "/ead/Q02",
-        hrefe: "https://moodle.utfpr.edu.br/mod/quiz/view.php?id=1930561",
-        start: "01-Sep-2025",
-        end: "08-Sep-2025",
-        descricao: "Processo de compilação, gravação e commit de código;",
-    },
-    {
-        quiz: "Q03",
-        hrefi: "/ead/Q03",
-        hrefe: "https://moodle.utfpr.edu.br/mod/quiz/view.php?id=1957479",
-        start: "15-Sep-2025",
-        end: "22-Sep-2025",
-        descricao: "Arquitetura de microcontroladores; Pinos de propósito geral (GPIO);",
-        //  descricao: "Interrupções via GPIO e Temporizadores (Timers);",
-    },
-    {
-        quiz: "Q04",
-        hrefi: "/ead/Q04",
-        hrefe: "https://moodle.utfpr.edu.br/mod/quiz/view.php?id=1957484",
-        start: "22-Sep-2025",
-        end: "29-Sep-2025",
-        descricao: "Interrupções via GPIO e Temporizadores (Timers);",
-        // descricao: "Módulo PWM; Conversor ADC;",
-    },
-    {
-        quiz: "Q05",
-        hrefi: "/ead/Q05",
-        hrefe: "https://moodle.utfpr.edu.br/mod/quiz/view.php?id=1957487",
-        start: "29-Sep-2025",
-        end: "06-Oct-2025",
-        descricao: "Módulo PWM; Conversor ADC;",
-        // descricao: "DMA: Direct memory access (Acesso direto à memória);",
-    },
-    {
-        quiz: "Q06",
-        hrefi: "/ead/Q06",
-        hrefe: "https://moodle.utfpr.edu.br/mod/quiz/view.php?id=1957569",
-        start: "06-Oct-2025",
-        end: "13-Oct-2025",
-        descricao: "DMA: Direct memory access (Acesso direto à memória);",
-        // descricao: "Uso do STM32CubeMonitor; Visualização de variáveis;",
-    },
-    {
-        quiz: "Q07",
-        hrefi: "/ead/Q07",
-        hrefe: "https://moodle.utfpr.edu.br/mod/quiz/view.php?id=1957590",
-        start: "13-Oct-2025",
-        end: "20-Oct-2025",
-        descricao: "Uso do STM32CubeMonitor; Visualização de variáveis;",
-        // descricao: "Uso do STM32CubeMonitor; MQTT, IoT;",
-    },
-    {
-        quiz: "Q08",
-        hrefi: "/ead/Q08",
-        hrefe: "https://moodle.utfpr.edu.br/mod/quiz/view.php?id=1957592",
-        start: "03-Nov-2025",
-        end: "10-Nov-2025",
-        descricao: "Uso do STM32CubeMonitor; MQTT, IoT;",
-        // descricao: "Uso do STM32CubeMonitor; InfluDB;",
-    },
-    {
-        quiz: "Q09",
-        hrefi: "/ead/Q09",
-        hrefe: "https://moodle.utfpr.edu.br/mod/quiz/view.php?id=1957596",
-        start: "10-Nov-2025",
-        end: "17-Nov-2025",
-        descricao: "Introdução ao PlatformIO;",
-    },
-    {
-        quiz: "Q10",
-        hrefi: "/ead/Q10",
-        hrefe: "https://moodle.utfpr.edu.br/mod/quiz/view.php?id=1957598",
-        start: "17-Nov-2025",
-        end: "24-Nov-2025",
-        // descricao: "FreeRTOS;",
-        descricao: "FreeRTOS: Tarefas, Filas e Semáforos;",
-    },
+  {
+    quiz: "Q00",
+    hrefi: "/ead/Q00",
+    hrefe: "https://moodle.utfpr.edu.br/course/section.php?id=384598",
+    start: "18-Aug-2025",
+    end: "25-Aug-2025",
+    descricao:
+      "Apresentação da Disciplina; Materiais utilizados na disciplina;",
+  },
+  {
+    quiz: "Q01",
+    hrefi: "/ead/Q01",
+    hrefe: "https://moodle.utfpr.edu.br/course/section.php?id=375304",
+    start: "25-Aug-2025",
+    end: "01-Sep-2025",
+    descricao:
+      "Sistemas Numéricos e Representação de Dados; Conceitos Básicos de Eletrônica;",
+  },
+  {
+    quiz: "Q02",
+    hrefi: "/ead/Q02",
+    hrefe: "https://moodle.utfpr.edu.br/course/section.php?id=375305",
+    start: "01-Sep-2025",
+    end: "08-Sep-2025",
+    descricao:
+      "Portas lógicas. Tabela Verdade. Mintermos e Maxtermos. Funções lógicas;",
+  },
+  {
+    quiz: "Q03",
+    hrefi: "/ead/Q03",
+    hrefe: "https://moodle.utfpr.edu.br/course/section.php?id=375306",
+    start: "15-Sep-2025",
+    end: "22-Sep-2025",
+    descricao:
+      "Teoremas fundamentais da álgebra de Boole. Teorema de Morgan e simplificação de funções lógicas.",
+  },
+  {
+    quiz: "Q04",
+    hrefi: "/ead/Q04",
+    hrefe: "https://moodle.utfpr.edu.br/course/section.php?id=375307",
+    start: "22-Sep-2025",
+    end: "29-Sep-2025",
+    descricao:
+      "Formas canônicas (mintermos e maxitermos), Mapas de Veitch Karnaugh;",
+  },
+  {
+    quiz: "Q05",
+    hrefi: "/ead/Q05",
+    hrefe: "https://moodle.utfpr.edu.br/course/section.php?id=375308",
+    start: "29-Sep-2025",
+    end: "06-Oct-2025",
+    descricao: "Exercícios sobre simplificação e introdução ao Arduino;",
+  },
+  {
+    quiz: "Q06",
+    hrefi: "/ead/Q06",
+    hrefe: "https://moodle.utfpr.edu.br/course/section.php?id=375309",
+    start: "06-Oct-2025",
+    end: "13-Oct-2025",
+    descricao: "Multiplexador e Demultiplexador;",
+  },
+  {
+    quiz: "Q07",
+    hrefi: "/ead/Q07",
+    hrefe: "https://moodle.utfpr.edu.br/course/section.php?id=375310",
+    start: "13-Oct-2025",
+    end: "20-Oct-2025",
+    descricao: "Códigos, codificadores e decodificadores;",
+  },
+  {
+    quiz: "Q08",
+    hrefi: "/ead/Q08",
+    hrefe: "https://moodle.utfpr.edu.br/course/section.php?id=391745",
+    start: "03-Nov-2025",
+    end: "10-Nov-2025",
+    descricao: "Arduino - Parte 02;",
+  },
+  {
+    quiz: "Q09",
+    hrefi: "/ead/Q09",
+    hrefe: "https://moodle.utfpr.edu.br/course/section.php?id=375314",
+    start: "10-Nov-2025",
+    end: "17-Nov-2025",
+    descricao: "Flip flop RS e JK;",
+  },
+  {
+    quiz: "Q10",
+    hrefi: "/ead/Q10",
+    hrefe: "https://moodle.utfpr.edu.br/course/section.php?id=375315",
+    start: "17-Nov-2025",
+    end: "24-Nov-2025",
+    descricao: "Flip flop JK, T e D;",
+  },
 
+  {
+    quiz: "Q11",
+    hrefi: "/ead/Q11",
+    hrefe: "https://moodle.utfpr.edu.br/course/section.php?id=375316",
+    start: "17-Nov-2025",
+    end: "24-Nov-2025",
+    descricao: "Contadores Assíncronos;",
+  },
+  {
+    quiz: "Q12",
+    hrefi: "/ead/Q12",
+    hrefe: "https://moodle.utfpr.edu.br/course/section.php?id=375317",
+    start: "17-Nov-2025",
+    end: "24-Nov-2025",
+    descricao: "Contadores Síncronos;",
+  },
+  {
+    quiz: "Q13",
+    hrefi: "/ead/Q13",
+    hrefe: "https://moodle.utfpr.edu.br/course/section.php?id=375312",
+    start: "17-Nov-2025",
+    end: "24-Nov-2025",
+    descricao: "Aritmética binaria, somador e subtrator;",
+  },
+  {
+    quiz: "Q14",
+    hrefi: "/ead/Q14",
+    hrefe: "https://moodle.utfpr.edu.br/course/section.php?id=375318",
+    start: "17-Nov-2025",
+    end: "24-Nov-2025",
+    descricao: "Conversão análogico-digital (CAD) e digital-analógico (CDA);",
+  },
+  {
+    quiz: "Q15",
+    hrefi: "/ead/Q15",
+    hrefe: "https://moodle.utfpr.edu.br/course/section.php?id=375319",
+    start: "17-Nov-2025",
+    end: "24-Nov-2025",
+    descricao: "Tipos de memórias digitais, registradores e geradores de clock;",
+  },
 ];
