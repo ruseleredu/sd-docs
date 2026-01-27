7ff53fa0e3e1296d1182905a9fce72cd38b92267
Adriano Ruseler <adrianoruseler@gmail.com>
2026-01-27 20:31:24 -0300

    Update project's social card

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -193,7 +193,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
+    image: "img/Banner-ELT72B.png",
     colorMode: {
       respectPrefersColorScheme: true,
     },
